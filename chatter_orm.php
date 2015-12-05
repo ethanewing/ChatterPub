5fea2d7fa75b2a438df8fc11d763b75b4b2aa65d
ethanewing <ethanewing52@gmail.com>
2015-12-05 12:43:05 -0500

    Update chatter_orm.php
    Added simpler Post class to work with until we have a better idea of how everything here works.

--- chatter_orm.php
+++ chatter_orm.php
@@ -6,6 +6,8 @@
 	 * is anonymous or not (should we choose to have user profiles).  So as of now this is unfinished.
 	 */
 
+/* 	We'll use a simpler version of Post until everything on server side is figured out.  The simpler class is below
+	this commented out one.
 	class Post {
 		private $id;
 		private $message;
@@ -123,6 +125,103 @@
 		      			'threadid' => $this->threadid);
     			return json_encode($json_obj);
 		}
+	}
+	*/
+	
+	class Post {
+		private $id;
+		private $message;
+		private $time;
+
+		public static function create($message, $time) {
+			$conn = new mysqli("classroom.cs.unc.edu", "eewing", "CH@ngemenow99Please!eewing", "eewingdb");
+			if ($conn->connect_errno)
+				echo "Failed to connect to MySQL: (" . $conn->connect_errno . ") " . $conn->connect_error;
+			else {
+				$result = $conn->query("insert into Post values (0, '" . $mesage . "', '" . $time . ")");
+				if($result) {
+					$newid = $conn->insert_id;
+					return new Post($newid, $message, $time);
+				}
+				
+				return null;
+			}
+		}
+		
+		public static function findByID($id) {
+			$conn = new mysqli("classroom.cs.unc.edu", "eewing", "CH@ngemenow99Please!eewing", "eewingdb");
+			if($conn->connect_errno)
+				echo "Failed to connect to MySQL: (" . $conn->connect_errno . ")" . $conn->connect_error;
+			else {
+				$result = $conn->query("select * from Post where id = " . $id);
+				if($result) {
+					if($result->num_rows == 0)
+						return null;
+					$post_info = $result->fetch_assoc();
+					return new Post($post_info['id'], 
+									$post_info['message'],
+									$post_info['time']);
+				}
+				
+				return null;
+			}
+		}
+		
+		/* Used to get multiple Post objects */
+		public static function getRange($start, $end) {
+			if ($start < 0) {
+				if ($end > $start)
+					return null;
+				$direction = "DESC";
+				$start *= -1;
+				$end *= -1;
+			} 
+			else {
+				if ($end < $start)
+					return null;
+				$direction = "ASC";
+			}
+			
+			$conn = new mysqli("classroom.cs.unc.edu", "eewing", "CH@ngemenow99Please!eewing", "eewingdb");
+			if($conn->connect_errno)
+				echo "Failed to connect to MySQL: (" . $conn->connect_errno . ")" . $conn->connect_error;
+			else {
+				$result = $conn->query("select id from Post order by id " . $direction);
+				$posts = array();
+			
+				if ($result) {
+					for ($i = 1; $i < $start; $i++)
+						$result->fetch_row();
+					
+					for ($i = $start; $i <= $end; $i++) {
+						$next_row = $result->fetch_row();
+						if ($next_row)
+							$posts[] = Post::findByID($next_row[0]);
+					}
+				}
+			}
+			
+			return $posts;
+		}
+		
+		private function __construct($id, $message, $time) {
+			$this->$id = $id;
+			$this->$message = $message;
+			$this->$time = $time;
+		}
+		
+		public function getID() {
+			return $this->$id;
+		}
+		
+		public function getMessage() {
+			return $this->$message;
+		}
+		
+		public function getTime() {
+			return $this->$time;
+		}
+
 	}
 	
 	class User {
