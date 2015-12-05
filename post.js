8f5b31629eb4d8eeeb553736f9351d6e45318f15
ethanewing <ethanewing52@gmail.com>
2015-12-05 13:48:47 -0500

    Create post.js
    JavaScript file for Post object; used for Post creation on front end

---
+++ post.js
@@ -0,0 +1,21 @@
+var Post = function(post_json) {
+  this.id = post_json.id;
+  this.message = post_json.id;
+  this.time = post_json.time;
+}
+
+Post.prototype.makeDiv = function(width) {
+  var post_div = $("<div>" + this.message + "</div>").css({
+	  position: "relative",
+	  height: "50px",
+	  width: width,
+	  "font-size": Post.FONT_SIZE,
+	  "margin-top": "5px"
+	});
+	
+	post_div.data('post', this);
+	
+	return post_div;
+};
+
+Post.FONT_SIZE = 14;
